45537d5b8cd55b57caa82a3f9dd9e0e75d63306b
Ibrahimi Egzon <Egzon.Ibrahimi@htl-wels.at>
2024-10-04 15:09:10 +0200

    Update main.c
    beginn

--- User/Src/main.c
+++ User/Src/main.c
@@ -17,14 +17,13 @@
 #include "stm32f429i_discovery_ts.h"
 #include "ts_calibration.h"
 
+
 /* Private includes ----------------------------------------------------------*/
-
 /* Private typedef -----------------------------------------------------------*/
-
 /* Private define ------------------------------------------------------------*/
 
 /* Private variables ---------------------------------------------------------*/
-
+ volatile int ChooseTimer = 0;
 /* Private function prototypes -----------------------------------------------*/
 static int GetUserButtonPressed(void);
 static int GetTouchState (int *xCoord, int *yCoord);
@@ -41,93 +40,121 @@ void SysTick_Handler(void)
  * @brief  The application entry point.
  * @retval int
  */
-int main(void)
+void EXTI0_IRQHandler(void)
 {
-	/* MCU Configuration--------------------------------------------------------*/
-	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-	HAL_Init();
-	/* Configure the system clock */
-	SystemClock_Config();
+	 __HAL_GPIO_EXTI_CLEAR_IT(GPIO_PIN_0);
+	 ChooseTimer = !ChooseTimer;
 
-	/* Initialize LCD and touch screen */
-	LCD_Init();
-	TS_Init(LCD_GetXSize(), LCD_GetYSize());
-	/* touch screen calibration */
-	//	TS_Calibration();
-
-	/* Clear the LCD and display basic starter text */
-	LCD_Clear(LCD_COLOR_BLACK);
-	LCD_SetTextColor(LCD_COLOR_YELLOW);
-	LCD_SetBackColor(LCD_COLOR_BLACK);
-	LCD_SetFont(&Font20);
-	// There are 2 ways to print text to screen: using printf or LCD_* functions
-	LCD_DisplayStringAtLine(0, "    HTL Wels");
-	// printf Alternative
-	LCD_SetPrintPosition(1, 0);
-	printf(" Fischergasse 30");
-	LCD_SetPrintPosition(2, 0);
-	printf("   A-4600 Wels");
-
-	LCD_SetFont(&Font8);
-	LCD_SetColors(LCD_COLOR_MAGENTA, LCD_COLOR_BLACK); // TextColor, BackColor
-	LCD_DisplayStringAtLineMode(39, "copyright xyz", CENTER_MODE);
-
-	int cnt = 0;
-	/* Infinite loop */
-	while (1)
+}
+	int main(void)
 	{
-		//execute main loop every 100ms
-		HAL_Delay(100);
+		/* MCU Configuration--------------------------------------------------------*/
+		/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+		HAL_Init();
+		/* Configure the system clock */
+		SystemClock_Config();
 
-		// display timer
-		cnt++;
+		/* Initialize LCD and touch screen */
+		LCD_Init();
+		TS_Init(LCD_GetXSize(), LCD_GetYSize());
+		/* touch screen calibration */
+		//	TS_Calibration();
+
+		/* Clear the LCD and display basic starter text */
+		LCD_Clear(LCD_COLOR_BLACK);
+		LCD_SetTextColor(LCD_COLOR_YELLOW);
+		LCD_SetBackColor(LCD_COLOR_BLACK);
 		LCD_SetFont(&Font20);
-		LCD_SetTextColor(LCD_COLOR_BLUE);
-		LCD_SetPrintPosition(5, 0);
-		printf("   Timer: %.1f", cnt/10.0);
+		// There are 2 ways to print text to screen: using printf or LCD_* functions
+		LCD_DisplayStringAtLine(0, "    HTL Wels");
+		// printf Alternative
+		LCD_SetPrintPosition(1, 0);
+		printf(" Fischergasse 30");
+		LCD_SetPrintPosition(2, 0);
+		printf("   A-4600 Wels");
 
-		// test touch interface
-		int x, y;
-		if (GetTouchState(&x, &y)) {
-			LCD_FillCircle(x, y, 5);
+		LCD_SetFont(&Font8);
+		LCD_SetColors(LCD_COLOR_MAGENTA, LCD_COLOR_BLACK); // TextColor, BackColor
+		LCD_DisplayStringAtLineMode(39, "copyright xyz", CENTER_MODE);
+
+		GPIO_InitTypeDef user;
+		user.Alternate = 0;
+		user.Mode = GPIO_MODE_IT_RISING;
+		user.Pin = GPIO_PIN_0;
+		user.Pull = GPIO_PULLDOWN;
+		user.Speed = GPIO_SPEED_FAST;
+		HAL_GPIO_Init(GPIOA, &user);
+
+//		led.Pin = GPIO_PIN_13;
+//		HAL_GPIO_Init(GPIOG, &user);
+
+		HAL_NVIC_EnableIRQ(EXTI0_IRQn);
+
+		int cnt = 0;
+		int cnt2 = 0;
+		/* Infinite loop */
+		while (1)
+		{
+			//execute main loop every 100ms
+			HAL_Delay(100);
+
+			// display timer
+			cnt++;
+			cnt2++;
+			LCD_SetFont(&Font20);
+			LCD_SetTextColor(LCD_COLOR_BLUE);
+			LCD_SetPrintPosition(5, 0);
+			printf("   Timer: %.1f", cnt/10.0);
+			LCD_SetPrintPosition(7, 0);
+			printf("Other Timer: %.1f", cnt2/10.0);
+
+			if(ChooseTimer == 0){
+				cnt++;
+			}else if (ChooseTimer == 1) {
+				cnt2++;
+			}
+			// test touch interface
+			//		int x, y;
+			//		if (GetTouchState(&x, &y)) {
+			//			LCD_FillCircle(x, y, 5);
+			//		}
+
+
+		}
+	}
+
+	/**
+	 * Check if User Button has been pressed
+	 * @param none
+	 * @return 1 if user button input (PA0) is high
+	 */
+	static int GetUserButtonPressed(void) {
+		return (GPIOA->IDR & 0x0001);
+	}
+
+	/**
+	 * Check if touch interface has been used
+	 * @param xCoord x coordinate of touch event in pixels
+	 * @param yCoord y coordinate of touch event in pixels
+	 * @return 1 if touch event has been detected
+	 */
+	static int GetTouchState (int* xCoord, int* yCoord) {
+		void    BSP_TS_GetState(TS_StateTypeDef *TsState);
+		TS_StateTypeDef TsState;
+		int touchclick = 0;
+
+		TS_GetState(&TsState);
+		if (TsState.TouchDetected) {
+			*xCoord = TsState.X;
+			*yCoord = TsState.Y;
+			touchclick = 1;
+			if (TS_IsCalibrationDone()) {
+				*xCoord = TS_Calibration_GetX(*xCoord);
+				*yCoord = TS_Calibration_GetY(*yCoord);
+			}
 		}
 
-
+		return touchclick;
 	}
-}
-
-/**
- * Check if User Button has been pressed
- * @param none
- * @return 1 if user button input (PA0) is high
- */
-static int GetUserButtonPressed(void) {
-	return (GPIOA->IDR & 0x0001);
-}
-
-/**
- * Check if touch interface has been used
- * @param xCoord x coordinate of touch event in pixels
- * @param yCoord y coordinate of touch event in pixels
- * @return 1 if touch event has been detected
- */
-static int GetTouchState (int* xCoord, int* yCoord) {
-	void    BSP_TS_GetState(TS_StateTypeDef *TsState);
-	TS_StateTypeDef TsState;
-	int touchclick = 0;
-
-	TS_GetState(&TsState);
-	if (TsState.TouchDetected) {
-		*xCoord = TsState.X;
-		*yCoord = TsState.Y;
-		touchclick = 1;
-		if (TS_IsCalibrationDone()) {
-			*xCoord = TS_Calibration_GetX(*xCoord);
-			*yCoord = TS_Calibration_GetY(*yCoord);
-		}
-	}
-
-	return touchclick;
-}
 
 
